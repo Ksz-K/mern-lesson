09b2e0f19448815b025360d52311ef913fa953f0
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-01-28 19:49:10 +0100

    Initial file structure setup

---
+++ client/src/components/pages/NotFound/NotFoundPage.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
+class NotFound extends Component {
+  state = {
+    counter: 77777
+  };
+
+  componentDidMount = () => {
+    const intervalId = setInterval(this.countdown, 1000);
+    this.setState({ intervalId });
+  };
+
+  componentWillUnmount = () => {
+    clearInterval(this.state.intervalId);
+  };
+  countdown = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>
+          This way{" "}
+          <code style={{ color: "#cba" }}>{this.props.location.pathname}</code>{" "}
+          is to nowhere...
+        </h1>
+        <p> Redirect to homepage in {this.state.counter} seconds </p>
+        {!this.state.counter && <Redirect to="/" />}
+        <img
+          src="../../img/deadend.jpg"
+          className="card-img-top"
+          alt="They map"
+        />
+      </div>
+    );
+  }
+}
+
+export default NotFound;
