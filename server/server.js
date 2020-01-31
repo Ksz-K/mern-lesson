c12bb80ea922bc83c76e863ef56544e5d9c6f995
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-01-31 15:24:48 +0100

    hk

--- server/server.js
+++ server/server.js
@@ -43,8 +43,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, function() {
-  console.log(`Server is running on port: ${PORT}`);
-app.listen(config.PORT, function() {
-  console.log("Server is running on Port:", config.PORT);
+  console.log(`Server is running on Port: ${PORT}`);
 });
